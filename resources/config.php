31bdc3cd6b291cbab4f66fc1e3d71000ff226076
astratow <davno_pora@mail.com>
2021-12-28 07:33:45 +0000

    removed extra files, added config.php

--- resources/config.php
+++ resources/config.php
@@ -1,16 +1,29 @@
 <?php
-/*defines directory separator depending on OS*/
+
+
+ob_start();/* allows to send multiple requests*/
+
+session_start();
+
+/*below defines directory separator depending on OS*/
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
+
+
 defined("DB_HOST") ? null : define("DB_HOST","localhost");
 
 defined("DB_USER") ? null : define("DB_USER", "root");
 
+
 defined("DB_PASS") ? null : define("DB_PASS", "");
 
 defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
+
+$connection = msession_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+require_once*+("functions.php")
 ?>
